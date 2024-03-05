1bd65aad9286b754a3e32f4b7675eefafe8a7eb4
Ayushi <Ayushi@LAPTOP-4MSCU7SL>
2024-03-05 13:33:56 +0530

    Login-register

---
+++ AJT_Project_JobPortal/src/ajt_project_jobportal/AJT_Project_JobPortal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ajt_project_jobportal;
+
+/**
+ *
+ * @author Ayushi
+ */
+public class AJT_Project_JobPortal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
